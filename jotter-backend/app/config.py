5426615aa27c14c67a41ff09e466cc01e21cc177
awsoankur <ankurk20@iitk.ac.in>
2023-11-24 13:20:27 +0530

    fix server deployment env

--- jotter-backend/app/config.py
+++ jotter-backend/app/config.py
@@ -1,14 +1,16 @@
+from dotenv import load_dotenv
+import os
 from pydantic_settings import BaseSettings
 
-class Settings(BaseSettings):
-    database_hostname :str
-    database_port :str
-    database_password : str
-    database_name :str
-    secret_key :str
-    algorithm : str
-    access_token_expiration_time_minutes : int
-    class Config():
-        env_file=".env"
+load_dotenv()
+class Settings():
+    database_hostname = os.environ.get("DATABASE_HOSTNAME")
+    database_port = os.environ.get("DATABASE_PORT")
+    database_password = os.environ.get("DATABASE_PASSWORD")
+    database_name = os.environ.get("DATABASE_NAME")
+    secret_key = os.environ.get("SECRET_KEY")
+    algorithm = os.environ.get("ALGORITHM")
+    access_token_expiration_time_minutes = int(os.environ.get("ACCESS_TOKEN_EXPIRATION_TIME_MINUTES"))
+    base_url = os.environ.get("BASE_URL")
 
 settings = Settings()
